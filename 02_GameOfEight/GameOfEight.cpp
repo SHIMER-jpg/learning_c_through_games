4b82a7d195a31cc353cefff4675de49534998d9e
Santiago DIaz <73915679+SHIMER-jpg@users.noreply.github.com>
2022-06-13 18:25:33 -0300

    added base structure for game of eight

---
+++ 02_GameOfEight/GameOfEight.cpp
@@ -0,0 +1,30 @@
+/*
+The Game of Eight
+
+------------------
+
+The game of 8 is played by 2 players
+
+The game starts with the initial number 8 (from here on refered to as the initial number)
+The first player will choose a number from 1 to 3 (from here on refered to as the chosen number)
+If the player inputs something outside of the 1 to 3 range it should be an error and the player must choose again
+The initial number is subtracted by the chosen number
+Then the next player's turn happens
+The next player cannot choose the number that the last player chose
+If the initial number goes to 0 on your turn, then you win
+Otherwise if the number goes below 0, your opponent wins
+The player should always see what choices are available to him when prompted to enter his choice
+After the game is over, the user should be prompted whether they want to play again
+
+
+Write a program to solve this problem
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}

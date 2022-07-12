87ea551e20a2836ac64b3859e01b6cb2119792f0
Santiago DIaz <73915679+SHIMER-jpg@users.noreply.github.com>
2022-07-12 18:18:56 -0300

    moved cheatsheet to MD

--- 03_PointerPractice/PointerPractice.cpp
+++ 03_PointerPractice/PointerPractice.cpp
@@ -3,44 +3,6 @@
 using namespace std;
 
 void Swap(int &x, int &y);
-
-/*
-CHEATSHEET:
-
- Pointers Store addresses of variables:
-
- int * pointerName; => Declares a int pointer type variable
-
- *pointerName => Access the variable stored in the pointer, known as de-referencing
-
- int * pointerName = nullptr; => Points to literally nothing, and cannot be de-referenced
-
- &variable;  => Returns a hex address, known as reference
-
- int x = 5;
- int * pointerName = &x; ==> Stores the address of the integer above, wouldn't work with other type
-
- int & intRef = x; => Creates a reference "intRef" to "x", which if altered, ends up altering x
-
- int & intRef2; => Throws an error, as references need to be defined, since they act like a constant
-
- intRef = 10; => would change the value of x to 10
-
-******************HERE IS WHERE IT GET'S MESSY****************
-
- const int * xPtr = &x; => Stores the address of x without being able to de-reference it later, yet being able to change where it points
- *xPtr = 10; => this cant be done
- xPtr = &y; => this can be done
-
- int * const yPtr = &y; => Can't change where it points but it can be de-referenced, this are basically references
- *yPtr = 19; => Works
- yPtr = &x; => Won't work
-
- const int & xRef = x; => Returns a reference that cannot be changed
- const int * const xPtr2 = &x; => Basically the same that above, but more complex.
-
-
-*/
 int main()
 {
     int x = 5;
